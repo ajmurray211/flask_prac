527edfa125e7cceb8592bf6ef27b6ec1c4de6ada
AJ Murray <aj.murr4y@gmail.com>
2022-08-19 17:59:07 -0400

    CRUD operations finished and tested with postman

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from distutils.log import debug
-from flask import Flask 
+from flask import Flask, jsonify, request, redirect, url_for
 from flask_pymongo import PyMongo
-
 
 app= Flask(__name__)
 # app.config['MONGO_URI'] = 'mongodb+srv://admin123:0000@cluster0.vzqog5g.mongodb.net/flask_prac?retryWrites=true&w=majority'
@@ -9,10 +7,41 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/flask_prac'
 
 mongo = PyMongo(app)
 
+@app.route('/', methods = ['GET'])
+def getAll():
+    holder = list()
+    currentCollection = mongo.db.flask_prac
+    for i in currentCollection.find():
+        holder.append({'name':i['name'], 'genre': i['favGenre'], 'game': i['favGame']})
+    return jsonify(holder)
 
-@app.route('/')
-def basic():
-    return
+@app.route('/<name>', methods = ['GET'])
+def getByName(name):
+    currentCollection = mongo.db.flask_prac
+    data = currentCollection.find_one({'name': name})
+    return jsonify({'name':data['name'], 'genre': data['favGenre'], 'game': data['favGame']})
+
+@app.route('/postData', methods = ['POST'])
+def postData():
+    currentCollection = mongo.db.flask_prac
+    name = request.json['name']
+    genre = request.json['genre']
+    game = request.json['game']
+    currentCollection.insert_one({'name': name, 'favGenre': genre, 'favGame': game})
+    return jsonify({'name': name, 'genre': genre, 'game': game})
+
+@app.route('/deleteData/<name>', methods = ['DELETE'])
+def deleteData(name):
+    currentCollection = mongo.db.flask_prac
+    currentCollection.delete_one({'name': name})
+    return redirect(url_for('getAll'))
+
+@app.route('/updateData/<name>', methods = ['PUT'])
+def updateData(name):
+    currentCollection = mongo.db.flask_prac
+    updatedName = request.json['name']
+    currentCollection.update_one({'name':name}, {'$set': {'name': updatedName}})
+    return redirect(url_for('getAll'))
 
 if __name__ == '__main__':
     app.run(debug =True)
